a56a50cded749b31c4667296014a62838f58bdd4
Asus <Asus@192.168.1.6>
2019-07-07 20:19:53 +0530

    final

---
+++ launchbrowser/src/launchbrowser/Chromebrowser.java
@@ -0,0 +1,17 @@
+package launchbrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chromebrowser {
+	
+	public static void main(String args[])
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Asus\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+			driver.get("https://www.google.com/");
+		
+	}
+
+}
