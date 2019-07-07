017d7fb9eedae82f928758142c621142a5647641
Asus <Asus@192.168.1.6>
2019-07-07 19:10:24 +0530

    First commit

---
+++ launchbrowser/src/launchbrowser/chromerlaunch.java
@@ -0,0 +1,22 @@
+
+package launchbrowser;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class chromerlaunch {
+	
+	public static void main(String args[]) throws InterruptedException
+	{
+	
+	WebDriver driver = new ChromeDriver();
+	System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Asus\\Downloads\\chromedriver.exe");
+	
+	driver.get("wwww.google.com");
+	driver.manage().window().maximize();
+	driver.close();
+	
+	}
+	
+
+}
